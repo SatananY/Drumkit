ab3659988c9f75c50279911488d28facbd810e29
SatananY <113567674+SatananY@users.noreply.github.com>
2022-11-17 19:18:07 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 for (var i = 0; i < document.querySelectorAll(".drum").length; i++) {
   document.querySelectorAll(".drum")[i].addEventListener('click', function() { //Arrow function
     // console.log(this.innerHTML)
+    this.style.color = "blue";
 
     var buttonInnerHTML = this.innerHTML;
     switch (buttonInnerHTML) {
@@ -47,8 +48,40 @@ for (var i = 0; i < document.querySelectorAll(".drum").length; i++) {
 );
 
 }
+document.addEventListener("keydown", function (event) {
+  console.log(event.key);
 
-document.addEventListener("keydown",  respondToKey (evt.key) );
-function respondToKey(evt) {
-console.log(evt.buttonInnerHTML);
-}
+  if(event.key == "w"){
+    var audio = new Audio('sounds/tom-1.mp3');
+    audio.play();
+  }
+    else if(event.key == "a"){
+      var audio = new Audio('sounds/tom-2.mp3');
+      audio.play();
+  }
+    else if(event.key == "s"){
+      var audio = new Audio('sounds/tom-3.mp3');
+      audio.play();
+  }
+    else if(event.key == "d"){
+      var audio = new Audio('sounds/tom-4.mp3');
+      audio.play();
+  }
+    else if(event.key == "j"){
+      var audio = new Audio('sounds/crash.mp3');
+      audio.play();
+  }
+    else if(event.key == "k"){
+      var audio = new Audio('sounds/kick-bass.mp3');
+      audio.play();
+    }
+    else if(event.key == "l"){
+      var audio = new Audio('sounds/snare.mp3');
+      audio.play();
+    }
+
+
+
+
+
+} );
